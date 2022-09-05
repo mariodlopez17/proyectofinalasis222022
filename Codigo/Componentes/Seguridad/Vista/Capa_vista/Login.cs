b10efa6984334913c5bd4341938839f516e166f0
unknown <jmmarroquinr00@gmail.com>
2022-09-04 22:15:16 -0600

    prototipo

---
+++ Codigo/Componentes/Seguridad/Vista/Capa_vista/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navegador_seg b = new Navegador_seg();
+            b.Show();
+            this.Hide();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                // TBcontraseña.PasswordChar = '*';
+                TBcontraseña.PasswordChar = '\0';
+            }
+            else
+                       if (TBcontraseña.Text != "")
+            {
+                // TBcontraseña.PasswordChar = '\0';
+                TBcontraseña.PasswordChar = '*';
+            }
+        }
+    }
+}
