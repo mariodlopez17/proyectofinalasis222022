91c4a90f1072f598e72665fea58dcfae51a23a2f
Diana Victores <dcpernillo@gmail.com>
2022-09-15 22:40:09 -0600

    subiendoprototiponofuncionalmantenimientos

---
+++ Codigo/Modulos/Contabilidad/ModuloContabilidadd/MantenimientoPolizaContable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModuloContabilidadd
+{
+    public partial class MantenimientoPolizaContable : Form
+    {
+        public MantenimientoPolizaContable()
+        {
+            InitializeComponent();
+        }
+    }
+}
