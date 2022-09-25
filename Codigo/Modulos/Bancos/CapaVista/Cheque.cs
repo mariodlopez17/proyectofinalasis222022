f4add009f85ec2681e3559f16711bd4af14d4c0c
Ester Lopez <esterlopez18@gmail.com>
2022-09-24 19:08:02 -0600

    Prototipo no funcional módulo bancario Generación de Cheque

---
+++ Codigo/Modulos/Bancos/CapaVista/Cheque.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class Cheque : Form
+    {
+        private static Cheque instancia = null;
+
+        public static Cheque ventana_unica()
+        {
+            if (instancia == null)
+            {
+                instancia = new Cheque();
+                return instancia;
+            }
+            return instancia;
+        }
+        public Cheque()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator Cheque(dummie v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
