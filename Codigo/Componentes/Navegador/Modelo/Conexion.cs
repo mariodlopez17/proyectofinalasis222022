88b81d540ee4d11815a54babfd0319db368dec7d
Esduardo Del Aguila <daguilae@yahoo.com>
2022-10-17 21:31:54 -0600

    corrección conexion en Navegador

--- Codigo/Componentes/Navegador/Modelo/Conexion.cs
+++ Codigo/Componentes/Navegador/Modelo/Conexion.cs
@@ -14,7 +14,7 @@ namespace NavegadorModelo
         public OdbcConnection conexion()
         {
             //creacion de la conexion via ODBC
-            OdbcConnection conn = new OdbcConnection("Dsn=Prueba1");
+            OdbcConnection conn = new OdbcConnection("Dsn=colchoneria");
             try
             {
                 conn.Open();
