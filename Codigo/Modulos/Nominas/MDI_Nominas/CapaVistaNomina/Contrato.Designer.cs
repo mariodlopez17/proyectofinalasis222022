39dcb95e23644a602dda11d054c1fccf277b0a7d
MiguelQuevec <mlunaq@miumg.edu.gt>
2022-10-21 12:49:20 -0600

    CRUD Contrato

--- Codigo/Modulos/Nominas/MDI_Nominas/CapaVistaNomina/Contrato.Designer.cs
+++ Codigo/Modulos/Nominas/MDI_Nominas/CapaVistaNomina/Contrato.Designer.cs
@@ -45,10 +45,11 @@ namespace CapaVistaNomina
             this.label3 = new System.Windows.Forms.Label();
             this.txtIDcontrato = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridContrato = new System.Windows.Forms.DataGridView();
+            this.navegador1 = new NavegadorVista.Navegador();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridContrato)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,19 +58,17 @@ namespace CapaVistaNomina
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1045, 246);
+            this.panel1.Size = new System.Drawing.Size(784, 56);
             this.panel1.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 20);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(16, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(188, 41);
+            this.label1.Size = new System.Drawing.Size(149, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "CONTRATO";
             // 
@@ -92,30 +91,27 @@ namespace CapaVistaNomina
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.txtIDcontrato);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Location = new System.Drawing.Point(47, 254);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Controls.Add(this.dataGridContrato);
+            this.panel2.Location = new System.Drawing.Point(35, 240);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(960, 436);
+            this.panel2.Size = new System.Drawing.Size(720, 338);
             this.panel2.TabIndex = 6;
             // 
             // txtestado
             // 
             this.txtestado.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txtestado.Location = new System.Drawing.Point(171, 322);
-            this.txtestado.Margin = new System.Windows.Forms.Padding(4);
+            this.txtestado.Location = new System.Drawing.Point(128, 262);
             this.txtestado.Name = "txtestado";
-            this.txtestado.Size = new System.Drawing.Size(105, 26);
+            this.txtestado.Size = new System.Drawing.Size(80, 22);
             this.txtestado.TabIndex = 18;
             this.txtestado.Tag = "estado_contrato";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(85, 322);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Location = new System.Drawing.Point(64, 262);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(68, 21);
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
             this.checkBox1.TabIndex = 17;
             this.checkBox1.Text = "Activo";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -124,20 +120,18 @@ namespace CapaVistaNomina
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label8.Location = new System.Drawing.Point(21, 322);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(16, 262);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(50, 19);
+            this.label8.Size = new System.Drawing.Size(42, 13);
             this.label8.TabIndex = 16;
             this.label8.Text = "Estado";
             // 
             // txtTipodePago
             // 
             this.txtTipodePago.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txtTipodePago.Location = new System.Drawing.Point(21, 266);
-            this.txtTipodePago.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTipodePago.Location = new System.Drawing.Point(16, 216);
             this.txtTipodePago.Name = "txtTipodePago";
-            this.txtTipodePago.Size = new System.Drawing.Size(255, 26);
+            this.txtTipodePago.Size = new System.Drawing.Size(192, 22);
             this.txtTipodePago.TabIndex = 12;
             this.txtTipodePago.Tag = "tipoPago_contrato";
             // 
@@ -145,10 +139,9 @@ namespace CapaVistaNomina
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label6.Location = new System.Drawing.Point(21, 246);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(16, 200);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.Size = new System.Drawing.Size(76, 13);
             this.label6.TabIndex = 7;
             this.label6.Tag = "";
             this.label6.Text = "Tipo De Pago";
@@ -156,10 +149,9 @@ namespace CapaVistaNomina
             // txthorasMensuales
             // 
             this.txthorasMensuales.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txthorasMensuales.Location = new System.Drawing.Point(21, 207);
-            this.txthorasMensuales.Margin = new System.Windows.Forms.Padding(4);
+            this.txthorasMensuales.Location = new System.Drawing.Point(16, 168);
             this.txthorasMensuales.Name = "txthorasMensuales";
-            this.txthorasMensuales.Size = new System.Drawing.Size(255, 26);
+            this.txthorasMensuales.Size = new System.Drawing.Size(192, 22);
             this.txthorasMensuales.TabIndex = 13;
             this.txthorasMensuales.Tag = "cantdidadHorasMensuales_contrato";
             // 
@@ -167,10 +159,9 @@ namespace CapaVistaNomina
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label5.Location = new System.Drawing.Point(21, 187);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(16, 152);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(114, 19);
+            this.label5.Size = new System.Drawing.Size(95, 13);
             this.label5.TabIndex = 8;
             this.label5.Tag = "";
             this.label5.Text = "Horas Mensuales";
@@ -178,10 +169,9 @@ namespace CapaVistaNomina
             // txtTipoJornada
             // 
             this.txtTipoJornada.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txtTipoJornada.Location = new System.Drawing.Point(21, 148);
-            this.txtTipoJornada.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTipoJornada.Location = new System.Drawing.Point(16, 120);
             this.txtTipoJornada.Name = "txtTipoJornada";
-            this.txtTipoJornada.Size = new System.Drawing.Size(255, 26);
+            this.txtTipoJornada.Size = new System.Drawing.Size(192, 22);
             this.txtTipoJornada.TabIndex = 14;
             this.txtTipoJornada.Tag = "tipoJornada_contrato";
             // 
@@ -189,10 +179,9 @@ namespace CapaVistaNomina
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label4.Location = new System.Drawing.Point(21, 128);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(16, 104);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 19);
+            this.label4.Size = new System.Drawing.Size(91, 13);
             this.label4.TabIndex = 9;
             this.label4.Tag = "";
             this.label4.Text = "Tipo De Jornada";
@@ -200,10 +189,9 @@ namespace CapaVistaNomina
             // txtsalario
             // 
             this.txtsalario.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txtsalario.Location = new System.Drawing.Point(21, 89);
-            this.txtsalario.Margin = new System.Windows.Forms.Padding(4);
+            this.txtsalario.Location = new System.Drawing.Point(16, 72);
             this.txtsalario.Name = "txtsalario";
-            this.txtsalario.Size = new System.Drawing.Size(255, 26);
+            this.txtsalario.Size = new System.Drawing.Size(192, 22);
             this.txtsalario.TabIndex = 15;
             this.txtsalario.Tag = "salario_contrato";
             // 
@@ -211,20 +199,18 @@ namespace CapaVistaNomina
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label3.Location = new System.Drawing.Point(21, 69);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(16, 56);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(132, 19);
+            this.label3.Size = new System.Drawing.Size(111, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Salario Del Contrato";
             // 
             // txtIDcontrato
             // 
             this.txtIDcontrato.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.txtIDcontrato.Location = new System.Drawing.Point(21, 30);
-            this.txtIDcontrato.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIDcontrato.Location = new System.Drawing.Point(16, 24);
             this.txtIDcontrato.Name = "txtIDcontrato";
-            this.txtIDcontrato.Size = new System.Drawing.Size(255, 26);
+            this.txtIDcontrato.Size = new System.Drawing.Size(192, 22);
             this.txtIDcontrato.TabIndex = 5;
             this.txtIDcontrato.Tag = "pk_id_contrato";
             // 
@@ -232,53 +218,59 @@ namespace CapaVistaNomina
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.label2.Location = new System.Drawing.Point(21, 10);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(16, 8);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "ID Contrato";
             // 
-            // dataGridView1
+            // dataGridContrato
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dataGridContrato.AllowUserToAddRows = false;
+            this.dataGridContrato.AllowUserToDeleteRows = false;
+            this.dataGridContrato.AllowUserToResizeColumns = false;
+            this.dataGridContrato.AllowUserToResizeRows = false;
+            this.dataGridContrato.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(320, 20);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.Size = new System.Drawing.Size(619, 407);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.Tag = "tbl_contrato";
+            this.dataGridContrato.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridContrato.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridContrato.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridContrato.Location = new System.Drawing.Point(240, 16);
+            this.dataGridContrato.MultiSelect = false;
+            this.dataGridContrato.Name = "dataGridContrato";
+            this.dataGridContrato.ReadOnly = true;
+            this.dataGridContrato.RowHeadersVisible = false;
+            this.dataGridContrato.RowHeadersWidth = 51;
+            this.dataGridContrato.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridContrato.ShowEditingIcon = false;
+            this.dataGridContrato.Size = new System.Drawing.Size(464, 315);
+            this.dataGridContrato.TabIndex = 0;
+            this.dataGridContrato.Tag = "tbl_contrato";
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(96, 72);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(574, 152);
+            this.navegador1.TabIndex = 7;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
             // 
             // Contrato
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1045, 703);
+            this.ClientSize = new System.Drawing.Size(784, 589);
+            this.Controls.Add(this.navegador1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Contrato";
             this.Text = "Contrato";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridContrato)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -301,6 +293,7 @@ namespace CapaVistaNomina
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtIDcontrato;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridContrato;
+        private NavegadorVista.Navegador navegador1;
     }
 }
--- Codigo/Modulos/Nominas/MDI_Nominas/CapaVistaNomina/Contrato.cs
+++ Codigo/Modulos/Nominas/MDI_Nominas/CapaVistaNomina/Contrato.cs
@@ -22,5 +22,17 @@ namespace CapaVistaNomina
             AgregarContrato b = new AgregarContrato();
             b.Show();
         }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+            NavegadorVista.Navegador.idApp = "6004";
+            TextBox[] Grupotextbox = { txtIDcontrato, txtsalario, txtTipoJornada, txthorasMensuales, txtTipodePago, txtestado };
+            TextBox[] Idtextbox = { txtIDcontrato, txtsalario, txtTipoJornada, txthorasMensuales, txtTipodePago, txtestado };
+            navegador1.textbox = Grupotextbox;
+            navegador1.tabla = dataGridContrato;
+            navegador1.textboxi = Idtextbox;
+            navegador1.actual = this;
+            navegador1.cargar(dataGridContrato, Grupotextbox, "modulo_Nominas");
+        }
     }
 }
