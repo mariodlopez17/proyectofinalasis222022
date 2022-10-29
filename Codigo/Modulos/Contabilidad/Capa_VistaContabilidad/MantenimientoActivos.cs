a2d3e3dde3839f13649aba1cd3d72ace1389c56f
Amaya94 <jamayao@miumg.edu.gt>
2022-10-28 20:14:08 -0600

    Agregando Error de clases Conexion y Controlador

--- Codigo/Modulos/Contabilidad/Capa_VistaContabilidad/MantenimientoActivos.cs
+++ Codigo/Modulos/Contabilidad/Capa_VistaContabilidad/MantenimientoActivos.cs
@@ -27,145 +27,145 @@ namespace Capa_VistaContabilidad
 
         private void iconButton25_Click(object sender, EventArgs e)
         {
-            // Class1 Conexion = new Class1()
-            String codigo = txtCodigo.Text;
-            MySqlDataReader reader = null;
+            //// Class1 Conexion = new Class1()
+            //String codigo = txtCodigo.Text;
+            //MySqlDataReader reader = null;
 
-            string sql = "SELECT id, codigo, nombre, descripcion, precio_publico, existencias FROM productos WHERE codigo LIKE '" + codigo + "' LIMIT 1";
-            MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion(); 
-            conexionBD.Open();
+            //string sql = "SELECT id, codigo, nombre, descripcion, precio_publico, existencias FROM productos WHERE codigo LIKE '" + codigo + "' LIMIT 1";
+            ////MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion(); 
+            ////conexionBD.Open();
 
-            try
-            {
-                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
-                reader = comando.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        txtId.Text = reader.GetString(0);
-                        txtCodigo.Text = reader.GetString(1);
-                        txtNombre.Text = reader.GetString(2);
-                        txtDescripcion.Text = reader.GetString(3);
-                        txtPrecioPublico.Text = reader.GetString(4);
-                        txtExistencias.Text = reader.GetString(5);
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("No se encontraron registros");
-                }
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show("Error al buscar " + ex.Message);
-            }
-            finally
-            {
-                conexionBD.Close();
-            }
+            //try
+            //{
+            //    MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+            //    reader = comando.ExecuteReader();
+            //    if (reader.HasRows)
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            txtId.Text = reader.GetString(0);
+            //            txtCodigo.Text = reader.GetString(1);
+            //            txtNombre.Text = reader.GetString(2);
+            //            txtDescripcion.Text = reader.GetString(3);
+            //            txtPrecioPublico.Text = reader.GetString(4);
+            //            txtExistencias.Text = reader.GetString(5);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("No se encontraron registros");
+            //    }
+            //}
+            //catch (MySqlException ex)
+            //{
+            //    MessageBox.Show("Error al buscar " + ex.Message);
+            //}
+            //finally
+            //{
+            //    conexionBD.Close();
+            //}
         }
 
         private void iconButton3_Click(object sender, EventArgs e)
         {
-            try
-            {
-                String codigo = txtCodigo.Text;
-                String nombre = txtNombre.Text;
-                String descripcion = txtDescripcion.Text;
-                double precio_publico = double.Parse(txtPrecioPublico.Text);
-                int existencias = int.Parse(txtExistencias.Text);
+            //try
+            //{
+            //    String codigo = txtCodigo.Text;
+            //    String nombre = txtNombre.Text;
+            //    String descripcion = txtDescripcion.Text;
+            //    double precio_publico = double.Parse(txtPrecioPublico.Text);
+            //    int existencias = int.Parse(txtExistencias.Text);
 
-                if (codigo != "" && nombre != "" && descripcion != "" && precio_publico > 0 && existencias > 0)
-                {
+            //    if (codigo != "" && nombre != "" && descripcion != "" && precio_publico > 0 && existencias > 0)
+            //    {
 
-                    string sql = "INSERT INTO productos (codigo, nombre, descripcion, precio_publico, existencias) VALUES ('" + codigo + "', '" + nombre + "','" + descripcion + "','" + precio_publico + "','" + existencias + "')";
+            //        string sql = "INSERT INTO productos (codigo, nombre, descripcion, precio_publico, existencias) VALUES ('" + codigo + "', '" + nombre + "','" + descripcion + "','" + precio_publico + "','" + existencias + "')";
 
-                    MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
-                    conexionBD.Open();
+            //        MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
+            //        conexionBD.Open();
 
-                    try
-                    {
-                        MySqlCommand comando = new MySqlCommand(sql, conexionBD);
-                        comando.ExecuteNonQuery();
-                        MessageBox.Show("Registro guardado");
-                        limpiar();
-                    }
-                    catch (MySqlException ex)
-                    {
-                        MessageBox.Show("Error al guardar: " + ex.Message);
-                    }
-                    finally
-                    {
-                        conexionBD.Close();
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Debe completar todos los campos");
-                }
-            }
-            catch (FormatException fex)
-            {
-                MessageBox.Show("Datos incorrectos: " + fex.Message);
-            }
+            //        try
+            //        {
+            //            MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+            //            comando.ExecuteNonQuery();
+            //            MessageBox.Show("Registro guardado");
+            //            limpiar();
+            //        }
+            //        catch (MySqlException ex)
+            //        {
+            //            MessageBox.Show("Error al guardar: " + ex.Message);
+            //        }
+            //        finally
+            //        {
+            //            conexionBD.Close();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Debe completar todos los campos");
+            //    }
+            //}
+            //catch (FormatException fex)
+            //{
+            //    MessageBox.Show("Datos incorrectos: " + fex.Message);
+            //}
         }
 
         private void iconButton26_Click(object sender, EventArgs e)
         {
-            String id = txtId.Text;
-            String codigo = txtCodigo.Text;
-            String nombre = txtNombre.Text;
-            String descripcion = txtDescripcion.Text;
-            double precio_publico = double.Parse(txtPrecioPublico.Text);
-            int existencias = int.Parse(txtExistencias.Text);
+            //String id = txtId.Text;
+            //String codigo = txtCodigo.Text;
+            //String nombre = txtNombre.Text;
+            //String descripcion = txtDescripcion.Text;
+            //double precio_publico = double.Parse(txtPrecioPublico.Text);
+            //int existencias = int.Parse(txtExistencias.Text);
 
-            string sql = "UPDATE productos SET codigo='" + codigo + "', nombre='" + nombre + "', descripcion='" + descripcion + "', precio_publico='" + precio_publico + "', existencias='" + existencias + "' WHERE id='" + id + "'";
+            //string sql = "UPDATE productos SET codigo='" + codigo + "', nombre='" + nombre + "', descripcion='" + descripcion + "', precio_publico='" + precio_publico + "', existencias='" + existencias + "' WHERE id='" + id + "'";
 
-            MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
-            conexionBD.Open();
+            //MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
+            //conexionBD.Open();
 
-            try
-            {
-                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
-                comando.ExecuteNonQuery();
-                MessageBox.Show("Registro modificado");
-                limpiar();
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show("Error al modificar: " + ex.Message);
-            }
-            finally
-            {
-                conexionBD.Close();
-            }
+            //try
+            //{
+            //    MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+            //    comando.ExecuteNonQuery();
+            //    MessageBox.Show("Registro modificado");
+            //    limpiar();
+            //}
+            //catch (MySqlException ex)
+            //{
+            //    MessageBox.Show("Error al modificar: " + ex.Message);
+            //}
+            //finally
+            //{
+            //    conexionBD.Close();
+            //}
         }
 
         private void iconButton12_Click(object sender, EventArgs e)
         {
-            String id = txtId.Text;
+            //String id = txtId.Text;
 
-            string sql = "DELETE FROM productos WHERE id='" + id + "'";
+            //string sql = "DELETE FROM productos WHERE id='" + id + "'";
 
-            MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
-            conexionBD.Open();
+            //MySqlConnection conexionBD = Capa_ControladorContabilidad.Class1.conexion();
+            //conexionBD.Open();
 
-            try
-            {
-                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
-                comando.ExecuteNonQuery();
-                MessageBox.Show("Registro eliminado");
-                limpiar();
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show("Error al eliminar: " + ex.Message);
-            }
-            finally
-            {
-                conexionBD.Close();
-            }
+            //try
+            //{
+            //    MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+            //    comando.ExecuteNonQuery();
+            //    MessageBox.Show("Registro eliminado");
+            //    limpiar();
+            //}
+            //catch (MySqlException ex)
+            //{
+            //    MessageBox.Show("Error al eliminar: " + ex.Message);
+            //}
+            //finally
+            //{
+            //    conexionBD.Close();
+            //}
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
